110be5d7be2f7e055ec24a7440ca26a41bed8c99
mtuan8820 <mtuan9517@gmail.com>
2022-10-14 09:24:38 +0700

    lab2_ex2 extend to 4 led7seg

--- lab2/Core/Src/main.c
+++ lab2/Core/Src/main.c
@@ -288,14 +288,17 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|EN0_Pin|EN1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, DOT_Pin|LED_RED_Pin|EN0_Pin|EN1_Pin
+                          |EN2_Pin|EN3_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, SEG0_Pin|SEG1_Pin|SEG2_Pin|SEG3_Pin
                           |SEG4_Pin|SEG5_Pin|SEG6_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_RED_Pin EN0_Pin EN1_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin|EN0_Pin|EN1_Pin;
+  /*Configure GPIO pins : DOT_Pin LED_RED_Pin EN0_Pin EN1_Pin
+                           EN2_Pin EN3_Pin */
+  GPIO_InitStruct.Pin = DOT_Pin|LED_RED_Pin|EN0_Pin|EN1_Pin
+                          |EN2_Pin|EN3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -314,29 +317,40 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 int counter=50;
-int state=0;
+int state=1;
 void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef * htim ){
 	counter--;
 	if(counter<=0){
 		counter=50;//reset counter
 		//turn off both led7seg
-		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
-		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
-
+		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
+		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
+		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
+		display7seg(state);
 		switch (state){
-		case 0://turn on first led7seg
-			state=1;
-			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
+		case 1://display 1 on first led
+			state=2;
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
 			break;
-		case 1: //turn on second led7seg
+		case 2://display 2 on second led
+			state=3;
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+			break;
+		case 3: //display 3 on third led
 			state=0;
-			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
+			break;
+		case 0: //display 0 on last one
+			state=1;
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
 			break;
 		default:
 			break;
 		}
-		display7seg(state+1);
+
 		HAL_GPIO_TogglePin ( LED_RED_GPIO_Port , LED_RED_Pin ) ;
+		HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
 	}
 }
 /* USER CODE END 4 */
